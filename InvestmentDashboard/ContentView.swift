ce3ebb067c31031077beb5362bbb9d935c1a7087
Owen1221111 <woen1221@gmail.com>
2025-09-25 12:21:08 +0800

    Initial Commit

---
+++ InvestmentDashboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InvestmentDashboard
+//
+//  Created by CheHung Liu on 2025/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InvestmentDashboard/InvestmentDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvestmentDashboardApp.swift
+//  InvestmentDashboard
+//
+//  Created by CheHung Liu on 2025/9/25.
+//
+
+import SwiftUI
+
+@main
+struct InvestmentDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
